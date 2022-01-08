d1e4e3b00988a69d589fa74414eed4c07c7019fe
harshitha811 <67686388+harshitha811@users.noreply.github.com>
2022-01-08 21:11:54 +0530

    Update lab10-forward-reasoning.py

--- lab10-forward-reasoning.py
+++ lab10-forward-reasoning.py
@@ -1,87 +1,104 @@
 import re
-def getPredicates(string):
-    expr = '[a-z~]+\([A-Za-z,]+\)'
-    return re.findall(expr, string)
+
+def isVariable(x):
+    return len(x) == 1 and x.islower() and x.isalpha()
 
 def getAttributes(string):
     expr = '\([^)]+\)'
     matches = re.findall(expr, string)
-    return [m for m in str(matches) if m.isalpha()]
-def DeMorgan(sentence):
-    string = ''.join(list(sentence).copy())
-    string = string.replace('~~','')
-    flag = '[' in string
-    string = string.replace('~[','')
-    string = string.strip(']')
-    for predicate in getPredicates(string):
-        string = string.replace(predicate, f'~{predicate}')
-    s = list(string)
-    for i, c in enumerate(string):
-        if c == '|':
-            s[i] = '&'
-        elif c == '&':
-            s[i] = '|'
-    string = ''.join(s)    
-    string = string.replace('~~','')
-    return f'[{string}]' if flag else string
-def Skolemization(sentence):
-    SKOLEM_CONSTANTS = [f'{chr(c)}' for c in range(ord('A'), ord('Z')+1)]
-    statement = ''.join(list(sentence).copy())
-    matches = re.findall('[∀∃].', statement)
-    for match in matches[::-1]:
-        statement = statement.replace(match, '')
-        statements = re.findall('\[\[[^]]+\]]', statement)
-        for s in statements:
-            statement = statement.replace(s, s[1:-1])
-        for predicate in getPredicates(statement):
-            attributes = getAttributes(predicate)
-            if ''.join(attributes).islower():
-                statement = statement.replace(match[1],SKOLEM_CONSTANTS.pop(0))
-            else:
-                aU = [a for a in attributes if not a.islower()][0]
-                statement = statement.replace(aU, f'{SKOLEM_CONSTANTS.pop(0)}({match[1]})')
-    return statement
-def fol_to_cnf(fol):
-    statement = fol.replace("<=>", "_")
-    while '_' in statement:
-        i = statement.index('_')
-        new_statement = '[' + statement[:i] + '=>' + statement[i+1:] + ']&['+ statement[i+1:] + '=>' + statement[:i] + ']'
-        statement = new_statement
-    statement = statement.replace("=>", "-")
-    expr = '\[([^]]+)\]'
-    statements = re.findall(expr, statement)
-    for i, s in enumerate(statements):
-        if '[' in s and ']' not in s:
-            statements[i] += ']'
-    for s in statements:
-        statement = statement.replace(s, fol_to_cnf(s))
-    while '-' in statement:
-        i = statement.index('-')
-        br = statement.index('[') if '[' in statement else 0
-        new_statement = '~' + statement[br:i] + '|' + statement[i+1:]
-        statement = statement[:br] + new_statement if br > 0 else new_statement
-    while '~∀' in statement:
-        i = statement.index('~∀')
-        statement = list(statement)
-        statement[i], statement[i+1], statement[i+2] = '∃', statement[i+2], '~'
-        statement = ''.join(statement)
-    while '~∃' in statement:
-        i = statement.index('~∃')
-        s = list(statement)
-        s[i], s[i+1], s[i+2] = '∀', s[i+2], '~'
-        statement = ''.join(s)
-    statement = statement.replace('~[∀','[~∀')
-    statement = statement.replace('~[∃','[~∃')
-    expr = '(~[∀|∃].)'
-    statements = re.findall(expr, statement)
-    for s in statements:
-        statement = statement.replace(s, fol_to_cnf(s))
-    expr = '~\[[^]]+\]'
-    statements = re.findall(expr, statement)
-    for s in statements:
-        statement = statement.replace(s, DeMorgan(s))
-    return statement
-def main():
-    statement = input("Enter FOL statement: ")
+    return matches
 
-    print(f"FOL converted to CNF: {Skolemization(fol_to_cnf(statement))}")
+def getPredicates(string):
+    expr = '([a-z~]+)\([^&|]+\)'
+    return re.findall(expr, string)
+class Fact:
+    def __init__(self, expression):
+        self.expression = expression
+        predicate, params = self.splitExpression(expression)
+        self.predicate = predicate
+        self.params = params
+        self.result = any(self.getConstants())
+        
+    def splitExpression(self, expression):
+        predicate = getPredicates(expression)[0]
+        params = getAttributes(expression)[0].strip('()').split(',')
+        return [predicate, params]
+    
+    def getResult(self):
+        return self.result
+    
+    def getConstants(self):
+        return [None if isVariable(c) else c for c in self.params]
+    
+    def getVariables(self):
+        return [v if isVariable(v) else None for v in self.params]
+    
+    def substitute(self, constants):
+        c = constants.copy()
+        f = f"{self.predicate}({','.join([constants.pop(0) if isVariable(p) else p for p in self.params])})"
+        return Fact(f)
+    
+class Implication:
+    def __init__(self, expression):
+        self.expression = expression
+        l = expression.split('=>')
+        self.lhs = [Fact(f) for f in l[0].split('&')]
+        self.rhs = Fact(l[1])
+        
+    def evaluate(self, facts):
+        constants = {}
+        new_lhs = []
+        for fact in facts:
+            for val in self.lhs:
+                if val.predicate == fact.predicate:
+                    for i, v in enumerate(val.getVariables()):
+                        if v:
+                          constants[v] = fact.getConstants()[i]
+                    new_lhs.append(fact)
+        predicate, attributes = getPredicates(self.rhs.expression)[0], str(getAttributes(self.rhs.expression)[0])
+        for key in constants:
+            if constants[key]:
+                attributes = attributes.replace(key, constants[key])
+        expr = f'{predicate}{attributes}'
+        return Fact(expr) if len(new_lhs) and all([f.getResult() for f in new_lhs]) else None
+
+class KB:
+    def __init__(self):
+        self.facts = set()
+        self.implications = set()
+            
+    def tell(self, e):
+        if '=>' in e:
+            self.implications.add(Implication(e))
+        else:
+            self.facts.add(Fact(e))
+        for i in self.implications:
+            res = i.evaluate(self.facts)
+            if res:
+                self.facts.add(res)
+
+    def query(self, e):
+        facts = set([f.expression for f in self.facts])
+        i = 1
+        print(f'Querying {e}:')
+        for f in facts:
+            if Fact(f).predicate == Fact(e).predicate:
+                print(f'\t{i}. {f}')
+                i += 1
+    
+    def display(self):
+        print("All facts: ")
+        for i, f in enumerate(set([f.expression for f in self.facts])):
+            print(f'\t{i+1}. {f}')
+def main():
+    kb = KB()
+    n = int(input("Enter number of statements in Knowledge Base: "))
+    
+    for _ in range(n):
+        kb.tell(input())
+        
+    print("Enter Query:")
+    query = input()
+    kb.query(query)
+    
+    kb.display()
